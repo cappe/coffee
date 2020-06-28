15f2079e2b33e0086390fa658380c82935cac2ce
Valtteri Rauhala <valtzu@gmail.com>
2020-06-28 22:37:16 +0300

    Set http keep-alive to 120 seconds

--- backend-frontend/http.js
+++ backend-frontend/http.js
@@ -2,9 +2,14 @@ import nodeFetch from "node-fetch";
 import http from 'http';
 import https from 'https';
 
+const agentOpts = {
+  keepAlive: true,
+  keepAliveMsecs: 120000,
+};
+
 const agents = {
-  'http:': new http.Agent({ keepAlive: true }),
-  'https:': new https.Agent({ keepAlive: true }),
+  'http:': new http.Agent(agentOpts),
+  'https:': new https.Agent(agentOpts),
 };
 
 const fetchOptions = {
